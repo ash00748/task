0b22c916fb4504f5ca95e8bf8079e704e8118db8
ash <anastasia.i.sharova@gmail.com>
2023-05-24 22:43:11 +0300

    beautiful comments

--- script.py
+++ script.py
@@ -1,36 +1,79 @@
 import sys
 import pandas as pd
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
 
-def excel_to_xml(file):
+
+def excel_to_xml(file: str, output_file: str):
+    """
+    функция, генерирующая xml файл на основе excel спредшита
+    Parameters
+    ----------
+    file : str
+        имя файла xlsx
+    output_file : str
+        имя файла xml, в который будет записан сгенерированный результат
+    """
     # Загружаем spreadsheet в объект pandas
-    df = pd.read_excel(file, sheet_name='Sheet1', header=4, 
-                    names=['CERTNO', 'CERTDATE', 'STATUS', 'IEC', 'EXPNAME', 'BILLID','SDATE', 'SCC', 'SVALUE'],
-                        converters={} )
-    head = pd.read_excel(file, nrows=3, names=['name', 'CERTDATA'], header=None, index_col=0, usecols="A:B")
+    # dataframe с основными даннами для работы
+    try:
+        df = pd.read_excel(
+            file,
+            sheet_name="Sheet1",
+            header=4,
+            names=[
+                "CERTNO",
+                "CERTDATE",
+                "STATUS",
+                "IEC",
+                "EXPNAME",
+                "BILLID",
+                "SDATE",
+                "SCC",
+                "SVALUE",
+            ],
+        )
 
-    #df=xl.copy()
-    #print(df.at['CERTDATE', 2])
-    root = ET.Element('CERTDATA') # Root элемент
-    tree=ET.ElementTree(root)
+    # dataframe - первые три строки с важной информацией (шапка)
+        head = pd.read_excel(
+            file,
+            nrows=3,
+            names=["name", "CERTDATA"],
+            header=None,
+            index_col=0,
+            usecols="A:B",
+        )
+    except FileNotFoundError:
+        print(f'Excel спредшит \"{file}\" не найден')
+        sys.exit(1)
+    root = ET.Element("CERTDATA")  # задаем root элемент
+    tree = ET.ElementTree(root)
 
-    filename=ET.SubElement(root, 'FILENAME')
-    filename.text = str(head.at['FileName', 'CERTDATA'])
-    envelope=ET.SubElement(root, 'ENVELOPE')
-    for row in df.index:
-        ecert = ET.SubElement(envelope, 'ECERT')
-        for column in df.columns: 
+    filename = ET.SubElement(root, "FILENAME")  # задаем элемент filename
+    filename.text = str(head.at["FileName", "CERTDATA"])
+
+    envelope = ET.SubElement(root, "ENVELOPE")  # задаем элемент envelope
+
+    for row in df.index:  # для каждой строки данных задаем элемент ecert
+        ecert = ET.SubElement(envelope, "ECERT")
+        for column in df.columns:  # для каждого поля данных создаем соотвествующий элемент
             entry = ET.SubElement(ecert, column)
-            data=df.at[row, column]
-            if column=='CERTDATE'or column=='SDATE': data=data.date()
-            if column=='IEC': data = f'{data:010d}'
-            if column=='EXPNAME':data='\"'+f'{data}'+'\"'
-            if column=='SVALUE' and data%10==0: data=int(data)
+            data = df.at[row, column]
+            if column == "CERTDATE" or column == "SDATE":
+                data = data.date()  # необходимое форматирование
+            if column == "IEC":
+                data = f"{data:010d}"
+            if column == "EXPNAME":
+                data = f"\"{data}\""
+            if column == "SVALUE" and data % 10 == 0:
+                data = int(data)
             entry.text = str(data)
 
+    # красиво записываем в файл
     ET.indent(tree, space="\t", level=0)
-    tree.write("test.xml", encoding="UTF-8", xml_declaration=True)
+    tree.write(f'{output_file}', encoding="UTF-8", xml_declaration=True)
 
-if __name__ == '__main__':
-    file_name=sys.argv[1]
-    excel_to_xml(file_name)
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    out_file = sys.argv[2]
+    excel_to_xml(file_name, out_file)
--- script2.py
+++ script2.py
@@ -1,50 +1,101 @@
 import sys
 import pandas as pd
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
 
-def excel_to_xml(file):
+
+def excel_to_xml(file: str, output_file: str):
+    """
+    функция, генерирующая xml файл на основе excel спредшита
+    Parameters
+    ----------
+    file : str
+        имя файла xlsx
+    output_file : str
+        имя файла xml, в который будет записан сгенерированный результат
+    """
     # Загружаем spreadsheet в объект pandas
-    df = pd.read_excel(file, sheet_name='Sheet1', header=4, 
-                    names=['CERTNO', 'CERTDATE', 'STATUS', 'IEC', 'EXPNAME', 'BILLID','SDATE', 'SCC', 'SVALUE']) #dataframe с основными даннами для работы
-    head = pd.read_excel(file, nrows=3, names=['name', 'CERTDATA'], header=None, index_col=0, usecols="A:B") #dataframe - первые три строки с важной информацией (шапка)
-    
-    #код валюты (доллары США)
-    code_usa='R01235'
+    # dataframe с основными даннами для работы
+    try:
+        df = pd.read_excel(
+            file,
+            sheet_name="Sheet1",
+            header=4,
+            names=[
+                "CERTNO",
+                "CERTDATE",
+                "STATUS",
+                "IEC",
+                "EXPNAME",
+                "BILLID",
+                "SDATE",
+                "SCC",
+                "SVALUE",
+            ],
+        )
 
-    root = ET.Element('CERTDATA') # Root элемент
-    tree=ET.ElementTree(root)
+    # dataframe - первые три строки с важной информацией (шапка)
+        head = pd.read_excel(
+            file,
+            nrows=3,
+            names=["name", "CERTDATA"],
+            header=None,
+            index_col=0,
+            usecols="A:B",
+        )
+    except FileNotFoundError:
+        print(f'Excel спредшит \"{file}\" не найден')
+        sys.exit(1)
 
-    filename=ET.SubElement(root, 'FILENAME')#задаем элемент filename
-    filename.text = str(head.at['FileName', 'CERTDATA'])
-    envelope=ET.SubElement(root, 'ENVELOPE')
-    for row in df.index:
-        ecert = ET.SubElement(envelope, 'ECERT')
-        for column in df.columns: 
+    # код валюты (доллары США)
+    code_usa = 'R01235'
+
+    root = ET.Element("CERTDATA")  # задаем root элемент
+    tree = ET.ElementTree(root)
+
+    filename = ET.SubElement(root, "FILENAME")  # задаем элемент filename
+    filename.text = str(head.at["FileName", "CERTDATA"])
+
+    envelope = ET.SubElement(root, "ENVELOPE")  # задаем элемент envelope
+
+    for row in df.index:  # для каждой строки данных задаем элемент ecert
+        ecert = ET.SubElement(envelope, "ECERT")
+        for column in df.columns:  # для каждого поля данных создаем соотвествующий элемент
             entry = ET.SubElement(ecert, column)
-            data=df.at[row, column]
-            if column=='CERTDATE'or column=='SDATE': data=data.date()
-            if column=='IEC': data = f'{data:010d}'
-            if column=='EXPNAME':data='\"'+f'{data}'+'\"'
-            if column=='SVALUE' and data%10==0: data=int(data)
+            data = df.at[row, column]
+            if column == "CERTDATE" or column == "SDATE":
+                data = data.date()  # необходимое форматирование
+            if column == "IEC":
+                data = f"{data:010d}"
+            if column == "EXPNAME":
+                data = f"\"{data}\""
+            if column == "SVALUE" and data % 10 == 0:
+                data = int(data)
             entry.text = str(data)
-        
-        date=(df.at[row,'SDATE']).date()
-        date=f'{date.day:02}/{date.month}/{date.year}'
-        svalue=df.at[row,'SVALUE']
 
-        url=f"http://www.cbr.ru/scripts/XML_daily.asp?date_req={date}"
-        xml_cbr=pd.read_xml(url,encoding='cp1251')
+        # переформатируем дату в формат, который используется в api cbr
+        date = (df.at[row, 'SDATE']).date()
+        date = f'{date.day:02}/{date.month:02}/{date.year}'
+        svalue = df.at[row, 'SVALUE']
 
-        index_usa=((xml_cbr.index [xml_cbr['ID']== code_usa]).to_list())[0]
-        value_usa=float((xml_cbr.at[index_usa,'Value']).replace(',','.'))
-        svalueus=round(svalue/value_usa,2)
+        url = f"http://www.cbr.ru/scripts/XML_daily.asp?date_req={date}"
+        xml_cbr = pd.read_xml(url, encoding='cp1251')
 
+        # индекс строки, соответствующей нужной валюте
+        index_usa = ((xml_cbr.index[xml_cbr['ID'] == code_usa]).to_list())[0]  
+        # курс нужной валюты лежит в формате str 123,123
+        value_usa = float((xml_cbr.at[index_usa, 'Value']).replace(',', '.'))
+        # вычисляем значение атрибута SVALUEUSD и округляем согласно требованиям
+        svalueus = round(svalue/value_usa, 2)
+        # добавляем новый атрибут в xml дерево
         entry = ET.SubElement(ecert, 'SVALUEUSD')
         entry.text = str(svalueus)
 
+    # красиво записываем в файл
     ET.indent(tree, space="\t", level=0)
-    tree.write("test2.xml", encoding="UTF-8", xml_declaration=True)
+    tree.write(f'{output_file}', encoding="UTF-8", xml_declaration=True)
 
-if __name__ == '__main__':
-    file_name=sys.argv[1]
-    excel_to_xml(file_name)
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    out_file = sys.argv[2]
+    excel_to_xml(file_name, out_file)
